5cf49195053de9a5a784b2bcc61ffffe0f5f5ad6
dazacher <dazacher2000@yahoo.com>
2021-06-19 05:36:35 -0700

    Using HostListener to make directive react to mouseover and mouseLeave event.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -6,10 +6,21 @@ import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 export class BetterHighlightDirective implements OnInit {
 
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
   }
 }
